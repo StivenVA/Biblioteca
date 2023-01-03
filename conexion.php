cfb287b517b0b6b6acaab82d4b0b44ff9d9cf499
Anggy21 <anggymichelle21@gmail.com>
2023-01-03 18:59:06 -0500

    usuarios

--- conexion.php
+++ conexion.php
@@ -1,3 +1,3 @@
 <?php
- $conect = pg_connect("host = 	babar.db.elephantsql.com dbname = tkqsjuch user = tkqsjuch password = Fsdzx65nB2IPxfJ5UZHIwTDbi9QTHX8z");
+ $conect = pg_connect("host = babar.db.elephantsql.com dbname = tkqsjuch user = tkqsjuch password = Fsdzx65nB2IPxfJ5UZHIwTDbi9QTHX8z");
 ?>
---
+++ log/loguearse.php
@@ -0,0 +1,22 @@
+<?php
+    include_once("../conexion.php");
+    session_start();
+    $user= $_POST['user'];
+    $pass= $_POST['contraseña'];
+    $query="select nomb_user from usuarios where nomb_user='$user' and cod_usuario='$pass'";
+    $consulta=pg_query($query);
+    $row=pg_fetch_object($consulta);
+    $cantidad=pg_num_rows($consulta);
+    if($cantidad>0){
+        $_SESSION['codigo_user']=$pass;
+        $_SESSION['nombre_user']=$row->nomb_user;
+        if($row->nomb_user=='ADMIN'){
+            header("location:../admin/admin.php");
+        }
+        else{
+           header("location:../selectCursos.php");
+        }   
+    }else{
+        header("location:fallo.php");
+    }
+ ?>
