deb4baee3b8e5e6d20936cb865bd2be23599cfd3
stivenVA <stivenv.ariza@gmail.com>
2023-01-05 01:13:47 -0500

    Opciones

--- biblioteca.php
+++ biblioteca.php
@@ -1,3 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+    <title>biblioteca</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css"/>
+</head>
 
-?>
+<body>
+        <div>
+            <form method="POST" action="/buscar/buscar.php">
+                <select name="cat">
+                    <option value=0></option>
+                    <option value=1>Titulo</option>
+                    <option value=2>Autor</option>
+                    <option value=3>Categoria</option>
+                    <option value=4>Editorial</option>
+                    
+                </select>
+        </div>
+</body>
+</html>
