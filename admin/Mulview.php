282359cf2728edcc588ad92f37fd8956dc719372
Anggy21 <anggymichelle21@gmail.com>
2023-01-21 15:46:37 -0500

    terminado

--- admin/Mulview.php
+++ admin/Mulview.php
@@ -10,7 +10,10 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../lib/bootstrap/css/bootstrap.min.css">
-    
+    <link rel="stylesheet" href="../estilos/admin.css">
+    <link rel="stylesheet" href="../estilos/tabla.css">
+    <link rel="stylesheet" href="../estilos/Mul.css">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
 
@@ -19,9 +22,12 @@ session_start();
     <title>Multas</title>
 </head>
 
-<div class="container"> 
-    
-</div>
+<body>
+
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
+  <img src="../imagenes/logounillanos.png" class="logouni"><h1 class="lines-effect">Multas</h1>
         <?php   
           $user=$_SESSION['codigo_user'];
           $sql="select nomb_user,estado,titulo,fecha_mul,motivo,deuda,m.cod_usuario,m.cod_ejem from multa m,usuarios u,ejemplar e,libros l where u.cod_usuario=m.cod_usuario and e.cod_ejem=m.cod_ejem and l.isbn=e.isbn";
@@ -32,29 +38,27 @@ session_start();
         ?>
         <div class="container">
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
+  <div class="nav">
+                <div class="link">
+                  <a href="admin.php">                           
+                    <span><?php echo $nomb->nomb_user;?></span>
+                    <ion-icon name="person-circle-outline"></ion-icon> 
+                  </a>
+                </div>
     
-    <form action="admin.php">
-            <button type="submit" class="btn btn-dark" >
-              <i class="fa-solid fa-user"></i> <?php echo $nomb->nomb_user;?>
-            </button>
-      </form>
-
-      <form action="Multas.php">
-      <form action="Multas.php">
-        <button type="submit" class="btn btn-dark">
-        <i class="fa-solid fa-hands"></i> Prestamos
-        </button>
-      </form>
-
-        <div class="collapse navbar-collapse" id="navbarNav">
-        <form action="coleccion.php">
-           <button type="submit" class="btn btn-dark">
-            <i class="fa-solid fa-bookmark"></i> Colección
-           </button>
-           </form>
-           </form>
-        </div>
+                <div class="link">
+                  <a href="Multas.php">
+                    <span>Prestamos</span>
+                    <ion-icon name="document-text-outline"></ion-icon>
+                  </a>
+                </div>  
+        
+                <div class="link">
+                  <a href="coleccion.php">
+                    <span>Colección</span>
+                    <ion-icon name="library-outline"></ion-icon>
+                  </a>
+                </div> 
 </div>
   </nav>
 </div>
@@ -90,9 +94,9 @@ session_start();
           <td><?php echo $mostrar->estado; ?></td>
           <td><?php echo $mostrar->fecha_mul; ?></td>
           <td><button type="button" id="Delete" class="btn btn-outline-success" data-bs-target="#Eliminar"  data-bs-toggle="modal"  data-cod="<?php echo $mostrar->cod_ejem; ?>" data-user="<?php echo $mostrar->cod_usuario; ?>" data-fecha="<?php echo $mostrar->fecha_mul; ?>" >
-          <i class="fa-solid fa-eraser"></i>Eliminar
+          <img src="../imagenes/goma-de-borrar.png" class="x">
             </button></td>
-            <td><button type="button" id="Edit" class="btn btn-outline-success" data-bs-target="#modal"  data-bs-toggle="modal"  data-cod="<?php echo $mostrar->cod_ejem; ?>" data-user="<?php echo $mostrar->cod_usuario; ?>" data-fecha="<?php echo $mostrar->fecha_mul; ?>" ><i class="fa-regular fa-pen-to-square"></i>Editar
+            <td><button type="button" id="Edit" class="btn btn-outline-success" data-bs-target="#modal"  data-bs-toggle="modal"  data-cod="<?php echo $mostrar->cod_ejem; ?>" data-user="<?php echo $mostrar->cod_usuario; ?>" data-fecha="<?php echo $mostrar->fecha_mul; ?>" ><img src="../imagenes/lapiz.png" class="x">
             </button></td>
         </tr>
       <?php } ?>
@@ -122,8 +126,8 @@ session_start();
           </p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-        <button type="submit" name='Modify' class="btn btn-primary" id="aceptar" >Aceptar</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><ion-icon name="close-circle-outline"></ion-icon>Cerrar</button>
+        <button type="submit" name='Modify' class="btn btn-primary" id="aceptar"><ion-icon name="checkmark-circle-outline"></ion-icon>Aceptar</button>
       </div>
           </form>
     </div>
@@ -143,8 +147,8 @@ session_start();
       <p>¿Está seguro de eliminar la multa?</p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-        <button type="submit" name='verificar' class="btn btn-primary" id="aceptar1" >Aceptar</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><ion-icon name="close-circle-outline"></ion-icon>Cerrar</button>
+        <button type="submit" name='verificar' class="btn btn-primary" id="aceptar1"><ion-icon name="checkmark-circle-outline"></ion-icon>Aceptar</button>
       </div>
           </form>
     </div>
